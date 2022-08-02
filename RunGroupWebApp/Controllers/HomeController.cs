c52938bbded5fe9a66eef174249e09ccf489a249
HP <HP@DESKTOP-5JKA4PO>
2022-08-02 18:22:40 +0100

    Add project files.

---
+++ RunGroupWebApp/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RunGroupWebApp.Helpers;
+using RunGroupWebApp.Interfaces;
+using RunGroupWebApp.Models;
+using RunGroupWebApp.ViewModels;
+using System.Diagnostics;
+using System.Globalization;
+using System.Net;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
+
+        public HomeController(ILogger<HomeController> logger, IClubRepository clubRepository )
+        {
+            _logger = logger;
+            _clubRepository = clubRepository;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var ipInfo = new IPInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url =  "https://ipinfo.io/129.56.80.181?token=5d47597bc2244b";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo myRII = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRII.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+                if(homeViewModel.City != null)
+                {
+                    homeViewModel.Clubs = await _clubRepository.GetClubByCity(homeViewModel.City);
+                }
+                else
+                {
+                    homeViewModel.Clubs = null;
+                }
+
+                return View(homeViewModel);
+            }
+
+            catch(Exception ex)
+            {
+                homeViewModel.Clubs = null;
+            }
+
+            return View(homeViewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RunGroupWebApp/Repository/RaceRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Interfaces;
+using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Repository
+{
+    public class RaceRepository : IRaceRepository
+    {
+        private readonly AppDbContext _context;
+
+        public RaceRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(Race race)
+        {
+
+            _context.Add(race);
+            return Save();
+        }
+
+        public bool Delete(Race race)
+        {
+            _context.Remove(race);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Race>> GetAll()
+        {
+            return await _context.Races.ToListAsync();
+        }
+
+        public  async Task<Race> GetByIdAsync(int id)
+        {
+            return await _context.Races.Include(i => i.Address).FirstOrDefaultAsync(x=> x.Id == id);
+
+        }
+
+        public async Task<Race> GetByIdAsyncNoTracking(int id)
+        {
+            return await _context.Races.Include(i => i.Address).AsNoTracking().FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Race race)
+        {
+
+            _context.Update(race);
+            return Save();
+        }
+    }
+}
